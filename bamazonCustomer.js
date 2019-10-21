245f4c1d736b5328e4c8aa43f444a2fcb3a5c833
Christopher Ruiz <chrisruizaz@gmail.com>
2019-10-20 21:56:12 -0700

    I figured out why my table wasnt working. added a git ignore file too

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,11 +2,53 @@ var mysql = require("mysql");
 let connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  database: "bamazon",
+  database: "bamazon_db",
   user: "root",
   password: "loveofcode"
 });
 
-connection.connect(err => {
-  connection.query("SELECT * FROM bamazon", err, res);
+connection.connect(function(err) {
+  console.log("connected as id " + connection.threadId);
+
+  seeProducts();
+  //   start();
 });
+function seeProducts() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      console.table(
+        res[i].id +
+          " | " +
+          res[i].product_name +
+          " | " +
+          res[i].department_name +
+          " | " +
+          res[i].price +
+          " | " +
+          res[i].stock_quantity
+      );
+    }
+    console.log("-----------------------------------");
+  });
+}
+
+// function start() {
+//   inquirer
+//     .prompt({
+//       name: "postOrBid",
+//       type: "list",
+//       message: "Would you like to [POST] an auction or [BID] on an auction?",
+//       choices: ["POST", "BID", "EXIT"]
+//     })
+//     .then(function(answer) {
+//       // based on their answer, either call the bid or the post functions
+//       if (answer.postOrBid === "POST") {
+//         postAuction();
+//       } else if (answer.postOrBid === "BID") {
+//         bidAuction();
+//       } else {
+//         connection.end();
+//       }
+//     });
+// }
